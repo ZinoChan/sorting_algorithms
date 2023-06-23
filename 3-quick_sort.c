84df62b4984911b3b437770f0480010ab9cdab9a
ZinoChan <devzinochan@gmail.com>
2023-06-23 15:18:11 +0100

    refactor

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,16 +1,6 @@
 #include "sort.h"
 
-/**
- * swaper - swapes
- * @a: arg
- * @b: arg
- */
-void swaper(int *a, int *b)
-{
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+
 
 
 /**
@@ -23,18 +13,23 @@ void swaper(int *a, int *b)
  */
 int partition(int *arr, int low, int high, size_t size)
 {
-	int pivot = arr[high], i = low - 1, j;
+	int pivot = arr[high], i = low - 1, j, tmp;
 
 	for (j = low; j <= high - 1; j++)
 	{
 		if (arr[j] < pivot)
 		{
 			i++;
-			swaper(&arr[i], &arr[j]);
-			print_array(arr, size);
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+			if (i != j)
+				print_array(arr, size);
 		}
 	}
-	swaper(&arr[i + 1], &arr[high]);
+	tmp = arr[i + 1];
+	arr[i + 1] = arr[high];
+	arr[high] = tmp;
 	print_array(arr, size);
 	return (i + 1);
 }
@@ -53,8 +48,10 @@ void quick_sort_helper(int *arr, int low, int high, size_t size)
 	if (low < high)
 	{
 		pivot = partition(arr, low, high, size);
-		quick_sort_helper(arr, low, pivot - 1, size);
-		quick_sort_helper(arr, pivot + 1, high, size);
+		if (pivot - low > 1)
+			quick_sort_helper(arr, low, pivot - 1, size);
+		if(high - pivot > 1)
+			quick_sort_helper(arr, pivot + 1, high, size);
 	}
 }
 
