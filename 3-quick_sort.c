659a043bb28e928693857b2536650be1b1578df1
ZinoChan <devzinochan@gmail.com>
2023-06-27 12:30:49 +0100

    refactor

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,59 +13,88 @@ void swaper(int *arr, size_t i, size_t j)
 	arr[i] = arr[j];
 	arr[j] = tmp;
 }
-/**
- * partition - selection----
- * @arr: arg
- * @low: arg
- * @high: arg
- * @size: size
- * Return: int
- */
-int partition(int *arr, int low, int high, size_t size)
-{
-	int pivot = arr[high], i = low - 1, j;
 
-	for (j = low; j <= high - 1; j++)
+/**
+ * swapAndPrint - Swaps two elements in an array and prints it
+ * @array: Pointer to the array.
+ * @i: Index of the first element to swap.
+ * @j: Index of the second element to swap.
+ * @arraySize: Size of the array.
+ * Return : void
+ */
+void swapAndPrint(int *array, size_t i, size_t j, size_t arraySize)
+{
+	if (i != j)
 	{
-		if (arr[j] < pivot)
+		swaper(array, i, j);
+		print_array(array, arraySize);
+	}
+}
+
+/**
+ * partitionArray - Partitions an array around a pivot element.
+ * @array: Pointer to the array to be partitioned.
+ * @startIdx: Starting index of the subarray to be partitioned.
+ * @endIdx: Ending index of the subarray to be partitioned.
+ * @arraySize: Size of the array.
+ * @pivotIdx: Pointer to store the index of the pivot
+ * Return : void
+ */
+void partitionArray(int *array, size_t startIdx, size_t endIdx,
+		size_t arraySize, size_t *pivotIdx)
+{
+	int pivot = array[endIdx];
+	size_t idx;
+	*pivotIdx = startIdx;
+
+	for (idx = startIdx; idx < endIdx; idx++)
+	{
+		if (array[idx] <= pivot)
 		{
-			i++;
-			swaper(arr, i, j);
-			if (i != j)
-				print_array(arr, size);
+			swapAndPrint(array, *pivotIdx, idx, arraySize);
+			(*pivotIdx)++;
 		}
 	}
-	swaper(arr, i + 1, high);
-	print_array(arr, size);
-	return (i + 1);
 }
 
 /**
-* q_sort_helper - recursive quick sort
-* @array: array to sort
-* @low: l----
-* @high: ------
-* @size: size
-**/
-void q_sort_helper(int *arr, int low, int high, size_t size)
-{
-	if (low < high)
-	{
-		int pivot = partition(arr, low, high, size);
-		q_sort_helper(arr, low, pivot - 1, size);
-		q_sort_helper(arr, pivot + 1, high, size);
-	}
-}
-
-/**
- * quick_sort - quick sort
+ * sort_helper -  helper func for quicksort algorithm.
  * @array: Pointer to the array to be sorted.
- * @size: Size of the array.
-*/
-void quick_sort(int *array, size_t size)
+ * @startIdx: Starting index of the subarray to be sorted.
+ * @endIdx: Ending index of the subarray to be sorted.
+ * @arraySize: Size of the array.
+ * Return: void
+ */
+void sort_helper(int *array, size_t startIdx, size_t endIdx, size_t arraySize)
 {
-	if (!array || size < 2)
+
+	size_t pivotIdx;
+
+	if (array == NULL || startIdx >= endIdx)
 		return;
 
-	q_sort_helper(array, 0, size - 1, size);
+	partitionArray(array, startIdx, endIdx, arraySize, &pivotIdx);
+	swapAndPrint(array, pivotIdx, endIdx, arraySize);
+
+	if (pivotIdx > startIdx + 1)
+		sort_helper(array, startIdx, pivotIdx - 1, arraySize);
+
+	if (endIdx > pivotIdx + 1)
+		sort_helper(array, pivotIdx + 1, endIdx, arraySize);
+}
+
+
+
+/**
+ * quick_sort - quicksort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array != NULL)
+	{
+		sort_helper(array, 0, size - 1, size);
+	}
 }
